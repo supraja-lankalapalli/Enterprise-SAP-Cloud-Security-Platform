e2394a74070f4ba53949605e519f264389d544a5
supraja-lankalapalli <i.suprajaLankalapalli@gmail.com>
2026-07-03 12:24:30 -0500

    Create portal landing page

---
+++ docs/portal/app.js
@@ -0,0 +1,9 @@
+document
+.getElementById("learnMoreBtn")
+.addEventListener("click", function(){
+
+    alert(
+        "Welcome to SL Cloud Technologies!\n\nEnterprise SAP Cloud Security Platform is under development."
+    );
+
+});
